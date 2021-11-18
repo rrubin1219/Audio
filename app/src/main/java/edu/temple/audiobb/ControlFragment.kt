eedfde817d2f50511a3f9e7529a7a681b5392351
Rachel <tul52326@temple.edu>
2021-11-18 00:45:44 -0500

    Implemented PlayService

--- app/src/main/java/edu/temple/audiobb/ControlFragment.kt
+++ app/src/main/java/edu/temple/audiobb/ControlFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.SeekBar
+import edu.temple.audlibplayer.PlayerService
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +20,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ControlFragment : Fragment() {
+    private lateinit var playButton: ImageButton
+    private lateinit var pauseButton: ImageButton
+    private lateinit var stopButton: ImageButton
+    private lateinit var progressBar: SeekBar
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -29,12 +37,43 @@ class ControlFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control, container, false)
+        val layout = inflater.inflate(R.layout.fragment_control, container, false)
+        playButton = layout.findViewById(R.id.playButton)
+        pauseButton = layout.findViewById(R.id.pauseButton)
+        stopButton = layout.findViewById(R.id.stopButton)
+        progressBar = layout.findViewById(R.id.seekBar)
+
+        playButton.setOnClickListener{
+
+        }
+        pauseButton.setOnClickListener{
+
+        }
+        stopButton.setOnClickListener{
+
+        }
+        progressBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        return layout
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
     companion object {
